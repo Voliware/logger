78331dd90463cb36e7372a6992b78b7e195665c6
tones31 <tones31@users.noreply.github.com>
2019-09-15 19:51:28 -0400

    1.4.3
    - now uses @voliware/node-build

--- build.js
+++ build.js
@@ -1,25 +1,6 @@
-const uglify = require('uglify-es');
-const fs = require('fs');
-
+const NodeBuild = require('@voliware/node-build');
+const version = "1.4.3";
+const name = "Logger";
 const input = './src/logger.js';
 const output = './dist/logger.min.js';
-
-fs.readFile(input, function(err, code){
-    if(err) throw err;
-    let result = uglify.minify(code.toString());
-    fs.writeFile(output, result.code, function(err){
-        let status = "OK";
-        if(err){
-           status = "ERR";
-           console.error(err);
-        } 
-
-        console.log(`\r\n\\\\     \/\/ \/\/\/\/\/\/ \/\/     \/\/ \\\\           \/\/ \/\/\\ \\\\\\\\\\\\ \\\\\\\\\\\\\\\r\n \\\\   \/\/ \/\/  \/\/ \/\/     \/\/   \\\\   \/\/\\   \/\/ \/\/ \\\\ \\\\  \\\\ \\\\___\r\n  \\\\ \/\/ \/\/  \/\/ \/\/     \/\/     \\\\ \/\/ \\\\ \/\/ \/\/   \\\\ \\\\\\\\\\  \\\\\r\n   \\\\\/ \/\/\/\/\/\/ \/\/\/\/\/\/ \/\/       \\\\\/   \\\\\/ \/\/     \\\\ \\\\  \\\\ \\\\\\\\\\\\`);
-        console.log(`
-            - ${(new Date()).toLocaleString()}
-            - INPUT: ${input}
-            - OUTPUT: ${output}
-            - STATUS: ${status}
-        `);
-    });
-});
+new NodeBuild.Build({name, version, input, output}).run();
--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
+const Assert = require('assert');
+const Fs = require('fs');
 const Logger = require('../index');
-const assert = require('assert')
-const fs = require('fs');
 
 const testfile = "./log.txt";
 try {
-    fs.accessSync(testfile);
-    fs.unlinkSync(testfile);
+    Fs.accessSync(testfile);
+    Fs.unlinkSync(testfile);
 }
 catch(e){
     
@@ -15,77 +15,77 @@ catch(e){
 it('sets the name from constructor options', () => {
     let name = "App";
     let logger = new Logger(name);
-    assert.strictEqual(logger.name, name);
+    Assert.strictEqual(logger.name, name);
 });
 
 it('sets the level from constructor options', () => {
     let level = Logger.level.error;
     let logger = new Logger("App", {level});
-    assert.strictEqual(logger.options.level, level);
+    Assert.strictEqual(logger.options.level, level);
 });
 
 it('sets the timestamp state from constructor options', () => {
     let logger = new Logger("App", {timestamp: {state: true}});
-    assert.strictEqual(logger.options.timestamp.state, true);
+    Assert.strictEqual(logger.options.timestamp.state, true);
 });
 
 it('sets the timestamp format from constructor options', () => {
     let format = Logger.timestamp.utc;
     let logger = new Logger("App", {timestamp: {format}});
-    assert.strictEqual(logger.options.timestamp.format, format);
+    Assert.strictEqual(logger.options.timestamp.format, format);
 });
 
 it('sets the context from constructor options', () => {
     let context = "User";
     let logger = new Logger("App", {context});
-    assert.strictEqual(logger.options.context, context);
+    Assert.strictEqual(logger.options.context, context);
 });
 
 it('creates a message at the verbose level', () => {
     let level = Logger.level.verbose;
     let logger = new Logger("App");
     let msg = logger.createMessage("Test", level);
-    assert.strictEqual(msg, "[VRB] [App] Test");
+    Assert.strictEqual(msg, "[VRB] [App] Test");
 });
 
 it('creates a message at the debug level', () => {
     let level = Logger.level.debug;
     let logger = new Logger("App");
     let msg = logger.createMessage("Test", level);
-    assert.strictEqual(msg, "[DBG] [App] Test");
+    Assert.strictEqual(msg, "[DBG] [App] Test");
 });
 
 it('creates a message at the info level', () => {
     let level = Logger.level.info;
     let logger = new Logger("App");
     let msg = logger.createMessage("Test", level);
-    assert.strictEqual(msg, "[INF] [App] Test");
+    Assert.strictEqual(msg, "[INF] [App] Test");
 });
 
 it('creates a message at the warning level', () => {
     let level = Logger.level.warning;
     let logger = new Logger("App");
     let msg = logger.createMessage("Test", level);
-    assert.strictEqual(msg, "[WRN] [App] Test");
+    Assert.strictEqual(msg, "[WRN] [App] Test");
 });
 
 it('creates a message at the error level', () => {
     let level = Logger.level.error;
     let logger = new Logger("App");
     let msg = logger.createMessage("Test", level);
-    assert.strictEqual(msg, "[ERR] [App] Test");
+    Assert.strictEqual(msg, "[ERR] [App] Test");
 });
 
 it('creates a message with no context', () => {
     let logger = new Logger("App");
     let msg = logger.createMessage("Test", Logger.level.info);
-    assert.strictEqual(msg, "[INF] [App] Test");
+    Assert.strictEqual(msg, "[INF] [App] Test");
 });
 
 it('creates a message with context', () => {
     let logger = new Logger("App", {context:"User"});
     let msg = logger.createMessage("Test", Logger.level.info);
-    assert.strictEqual(msg, "[INF] [App] [User] Test");
+    Assert.strictEqual(msg, "[INF] [App] [User] Test");
 });
 
 it('creates a message with a UTC timestamp', () => {
@@ -95,7 +95,7 @@ it('creates a message with a UTC timestamp', () => {
     let regex = /(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\,\s\d{2}\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{4}\s\d{2}:\d{2}:\d{2}\sGMT/;
     let date = msg.split(']')[0].replace('[', '').replace(']', '');
     let valid = regex.test(date);
-    assert.strictEqual(valid, true);
+    Assert.strictEqual(valid, true);
 });
 
 it('creates a log file if it does not exist', async () => {
@@ -106,7 +106,7 @@ it('creates a log file if it does not exist', async () => {
         }
     });
     await logger.info("Test");
-    assert.strictEqual(fs.existsSync(testfile), true);
+    Assert.strictEqual(Fs.existsSync(testfile), true);
 });
 
 it('appends to the log file', async () => {
@@ -118,9 +118,9 @@ it('appends to the log file', async () => {
     });
 
     await logger.info("Test");
-    let file = fs.readFileSync(testfile).toString();
+    let file = Fs.readFileSync(testfile).toString();
     let contents = '[INF] [App] Test\r\n[INF] [App] Test\r\n';
-    assert.strictEqual(file, contents);
+    Assert.strictEqual(file, contents);
 });
 
 it('erases the log file', async () => {
@@ -132,8 +132,8 @@ it('erases the log file', async () => {
     });
 
     await logger.eraseLogFile();
-    let file = fs.readFileSync(testfile).toString();
-    assert.strictEqual(file, '');
+    let file = Fs.readFileSync(testfile).toString();
+    Assert.strictEqual(file, '');
 });
 
 it('deletes the log file', async () => {
@@ -146,11 +146,11 @@ it('deletes the log file', async () => {
 
     await logger.deleteLogfile();
     try {
-        fs.accessSync(testfile);
-        assert.strictEqual(1, 0);
+        Fs.accessSync(testfile);
+        Assert.strictEqual(1, 0);
     }
     catch(e){
         // we want it to fail
-        assert.strictEqual(1, 1);
+        Assert.strictEqual(1, 1);
     }
 });
